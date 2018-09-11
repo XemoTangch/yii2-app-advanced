8395c063fe86306071ad9fe88c59d72439078c40
XemoTangch <jmphper@163.com>
2018-09-11 23:05:37 +0800

    edit rbac

--- console/migrations/m180904_144700_init_rbac.php
+++ console/migrations/m180904_144700_init_rbac.php
@@ -13,32 +13,41 @@ class m180904_144700_init_rbac extends Migration
     public function safeUp()
     {
         $auth = Yii::$app->authManager;
+//
+//        // 添加 "createPost" 权限
+//        $createPost = $auth->createPermission('createPost');
+//        $createPost->description = 'Create a post';
+//        $auth->add($createPost);
+//
+//        // 添加 "updatePost" 权限
+//        $updatePost = $auth->createPermission('updatePost');
+//        $updatePost->description = 'Update post';
+//        $auth->add($updatePost);
+//
+//        // 添加 "author" 角色并赋予 "createPost" 权限
+//        $author = $auth->createRole('author');
+//        $auth->add($author);
+//        $auth->addChild($author, $createPost);
+//
+//        // 添加 "admin" 角色并赋予 "updatePost"
+//        // 和 "author" 权限
+//        $admin = $auth->createRole('admin');
+//        $auth->add($admin);
+//        $auth->addChild($admin, $updatePost);
+//        $auth->addChild($admin, $author);
+//
+//        // 为用户指派角色。其中 1 和 2 是由 IdentityInterface::getId() 返回的id
+//        // 通常在你的 User 模型中实现这个函数。
+//        $auth->assign($admin, 1);
 
-        // 添加 "createPost" 权限
-        $createPost = $auth->createPermission('createPost');
-        $createPost->description = 'Create a post';
-        $auth->add($createPost);
-
-        // 添加 "updatePost" 权限
-        $updatePost = $auth->createPermission('updatePost');
-        $updatePost->description = 'Update post';
-        $auth->add($updatePost);
-
-        // 添加 "author" 角色并赋予 "createPost" 权限
-        $author = $auth->createRole('author');
-        $auth->add($author);
-        $auth->addChild($author, $createPost);
-
-        // 添加 "admin" 角色并赋予 "updatePost"
-        // 和 "author" 权限
-        $admin = $auth->createRole('admin');
-        $auth->add($admin);
-        $auth->addChild($admin, $updatePost);
-        $auth->addChild($admin, $author);
-
-        // 为用户指派角色。其中 1 和 2 是由 IdentityInterface::getId() 返回的id
-        // 通常在你的 User 模型中实现这个函数。
-        $auth->assign($admin, 1);
+        // 管理后台管理员权限
+        $manageAdmin = $auth->createPermission('manageAdmin');
+        $manageAdmin->description = '管理后台管理员';
+        
+        
+        // 添加超级管理员角色
+        $super_admin = $auth->createRole('superAdmin');
+        $auth->add($super_admin);
     }
 
     /**
